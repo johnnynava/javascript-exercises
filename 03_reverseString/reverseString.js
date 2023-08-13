fa3ca8c00a587666df85ce5fb04f087fb1e09859
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-13 21:49:55 +0100

    Finish Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+let string = "";
 
-};
+function reverseString(string) {
+    let array = string.split("");
+    array.reverse();
+    let reversed = array.join("");
+    return reversed;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
