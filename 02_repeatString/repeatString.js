3123154235f63f3e81a2d21e82e5385092ff6acb
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-13 21:31:01 +0100

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+let num = 0;
+let string = "";
 
-};
+function repeatString(string,num){
+    let outputString = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    else for (i=0; i<num; i++){
+        outputString += string;
+    }
+    return outputString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
