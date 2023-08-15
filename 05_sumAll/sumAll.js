34a81f644104c33efa7182e668668c5907a5286c
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-15 12:43:06 +0100

    Finish Exercise 5
    I think I like my solution better than the one provided by TOP.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+function sumAll(a,b){;
+    result = 0
+    if (a<0 || b< 0 || typeof a != typeof 1 || typeof b != typeof 1){
+        result = "ERROR";
+    }
+    else if (a>b){
+        for (i=b; i <= a; i++){
+            result += i;
+        }
+    }
+    else for (i=a; i <= b; i++){
+        result += i;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
