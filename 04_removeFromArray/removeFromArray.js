cacac48db639eb59220391b53ec29ad15bffba53
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-15 12:12:56 +0100

    Finish Exercise 4
    Took some time to finish this by myself but I was proud once I did it,
    I looked at the solution and see that I didn't even need to use a loop
    in order to achieve this.
    
    I could have used the forEach arraymethod or the includes+filter array
    method. (I used the includes array method alone)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(){
+    const args = Array.from(arguments);
+    let originalArray = args.shift();
+    for (i=0; i<args.length; i++){    
+        originalArray = originalArray.filter((word) => word !== args[i]);
+    }
+    return originalArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
