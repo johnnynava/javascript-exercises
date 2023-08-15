7cccfff7532da1930abdf6ac64c981da6a163286
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-15 13:23:11 +0100

    Finish Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(temperature){
+  return temperature = +((temperature-32)*(5/9)).toFixed(1);
+}
 
-const convertToFahrenheit = function() {
-};
+function convertToFahrenheit(temperature){
+  return temperature = +(temperature*(9/5)+32).toFixed(1);
+}
+
+convertToCelsius(32) // fahrenheit to celsius, should return 0
+
+convertToFahrenheit(0) // celsius to fahrenheit, should return 32
 
 // Do not edit below this line
 module.exports = {
