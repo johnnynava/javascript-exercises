d9c50e51ebd86df4f532f3d150f294ded1fd9724
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-21 16:47:44 +0100

    Finish Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
-};
+function findTheOldest(array){
+  const currentYear = new Date().getFullYear();
+  const alive = array.filter((object) => !object.yearOfDeath);
+  array.forEach((object) => {
+    if (!object.yearOfDeath){
+      object.yearOfDeath=currentYear;
+    }
+  });
+  if (!alive===[]){
+    alive.sort((a,b) => (a.yearOfDeath-a.yearOfBirth) > (b.yearOfDeath-b.yearOfBirth) ? -1 : 1);
+    return alive[0];
+  }
+  else {
+    array.sort((a,b) => (a.yearOfDeath-a.yearOfBirth) > (b.yearOfDeath-b.yearOfBirth) ? -1 : 1);
+    return array[0];
+  }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
