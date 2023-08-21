bde1163c51908b7938b69a1f322e0b1b33df7459
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-21 14:11:59 +0100

    Finish Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
-};
+function palindromes(word){
+    array = word.split("");
+    filteredArray = array.filter((character) =>
+        character !== (" ") && 
+        character !== (",") && 
+        character !== ("?") && 
+        character !== (".") &&
+        character !== ("!"));
+    filteredWord = (filteredArray.join("")).toLowerCase();
+    reversedWord = ((filteredArray.reverse()).join("")).toLowerCase();
+    return filteredWord === reversedWord; 
+}
 
 // Do not edit below this line
 module.exports = palindromes;
