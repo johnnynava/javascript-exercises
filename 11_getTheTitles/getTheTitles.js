8c8b9c4142a61a7416c71ede4c003454c76cf791
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-21 15:10:10 +0100

    Finish Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
-const getTheTitles = function() {
-
-};
+function getTheTitles(array){
+    return array.map((book) => book.title);
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
