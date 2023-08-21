f469b1ffe00ad286e67ceefe6a2250bdcab5659d
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-21 15:02:36 +0100

    Finish Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+function fibonacci(n){
+    if (n<0) return "OOPS";
+    else {
+    let startVal = 0;
+    let endVal = 1;
+    for (let i=2; i<=n; i++){
+        total = startVal + endVal;
+        startVal = endVal;
+        endVal = total;
+    }
+    return endVal;
+}
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
